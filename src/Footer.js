407845892a638d484bd5bc2f5b82f0859bd50e47
Edgar Florez <edgarfmail@gmail.com>
2025-06-06 05:56:13 -0500

    CSS - styling
        - create typefaces in the css rules.
        - organize grid for header and footer.

--- src/Footer.js
+++ src/Footer.js
@@ -2,12 +2,14 @@ import logo from "./assets/Logo.svg";
 
 export function Footer() {
   return (
-    <footer>
-      <div class="logo">
+    <footer className="grid footer">
+      <div class="footer__logo">
         <img src={logo} alt="logo" />
       </div>
-      <div class="doormat">
-        <h2>Dootmat Navigation</h2>
+      <div class="footer__doormat">
+        <h2 className="section-title footer__section-title">
+          Dootmat Navigation
+        </h2>
         <ul>
           <li>
             <a href="#home">Home</a>
@@ -29,8 +31,8 @@ export function Footer() {
           </li>
         </ul>
       </div>
-      <div className="contact">
-        <h2>Contact</h2>
+      <div className="footer__contact">
+        <h2 className="section-title footer__section-title">Contact</h2>
         <ul>
           <li>
             <a href="#address">Address</a>
@@ -43,8 +45,10 @@ export function Footer() {
           </li>
         </ul>
       </div>
-      <div className="social">
-        <h2>Social Media Links</h2>
+      <div className="footer__social">
+        <h2 className="section-title footer__section-title">
+          Social Media Links
+        </h2>
         <ul>
           <li>
             <a href="#address">Address</a>
--- src/Nav.js
+++ src/Nav.js
@@ -3,22 +3,34 @@ export function Nav() {
     <nav className="main-nav">
       <ul>
         <li>
-          <a href="./homepage.html">Home</a>
+          <a className="lead-text" href="./homepage.html">
+            Home
+          </a>
         </li>
         <li>
-          <a href="./about.html">About</a>
+          <a className="lead-text" href="./about.html">
+            About
+          </a>
         </li>
         <li>
-          <a href="./menu.html">menu</a>
+          <a className="lead-text" href="./menu.html">
+            menu
+          </a>
         </li>
         <li>
-          <a href="./reservations.html">reservations</a>
+          <a className="lead-text" href="./reservations.html">
+            reservations
+          </a>
         </li>
         <li>
-          <a href="./order-online.html">order online</a>
+          <a className="lead-text" href="./order-online.html">
+            order online
+          </a>
         </li>
         <li>
-          <a href="./login.html">login</a>
+          <a className="lead-text" href="./login.html">
+            login
+          </a>
         </li>
       </ul>
     </nav>
